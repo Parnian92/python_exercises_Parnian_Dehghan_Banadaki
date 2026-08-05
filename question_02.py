7990df71912415bc787b076db81849fe767de862
Parnian92 <parnian.db92@gmail.com>
2026-08-05 23:53:45 +0330

    first commit

---
+++ question_02.py
@@ -0,0 +1,9 @@
+#TA
+#2)
+answer=input('Salam! Aya mikhahid kharid konid?')
+answer=answer.strip().lower()
+if answer=='yes':
+    print('Yaddasht mikonam')
+else:
+    print('Besiyar ali')
+
---
+++ question_03.py
@@ -0,0 +1,10 @@
+#TA
+#3)
+answer=input('Salam! Aya mikhahid kharid konid?')
+answer==answer.strip().lower()
+if answer=='yes':
+    print('Yaddasht mikonam.')
+elif answer=='no':
+    print('Mamnoon')
+else:
+    print('Faghat ba yes va no javab dahid.')    
